f942fc8b8b8fb4f5d3cd6da6fe97d573065593c3
drewmard <arm286@cornell.edu>
2019-07-21 22:47:15 -0400

    “deconv“

--- scripts/GTEx_Deconv/cibersort_analysis.R
+++ scripts/GTEx_Deconv/cibersort_analysis.R
@@ -13,7 +13,8 @@ df.tis <- fread(paste0(workdir,'GTEx_v7_Annotations_SampleAttributesDS.txt'),dat
 # df.id <- fread(paste0(workdir,'GTEx_v7_genexpr_SPLIT.txt'),data.table = F)
 
 # deconvolution data:
-df.ciber <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/CIBERSORT_out/GTEx_v7_genexpr_ALL.CIBERSORT.ABS-',absolute,'.QN-F.perm-1000.txt'))
+f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/CIBERSORT_out/GTEx_v7_genexpr_ALL.CIBERSORT.ABS-',absolute,'.QN-F.perm-1000.DECONV_ONLY.txt')
+df.ciber <- fread(f)
 
 # pheno_num <- 1
 # f <- paste0(workdir,'CIBERSORT_out/','GTEx_v7_genexpr_',pheno_num,'.CIBERSORT.ABS-',abs,'.QN-F.perm-1000.txt')
@@ -24,21 +25,20 @@ df.ciber <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/CIBERSORT_out
 #   df.ciber <- rbind(df.ciber,df.ciber.tmp)
 # }
 
-
+# merge together deconvolution data w/ covariate data
 df.ciber.2 <- merge(df.ciber,df.tis[,c('SAMPID','SMTSD')],by.x='Input Sample',by.y = 'SAMPID')
 paste.s <- function(x) {return(paste(x[1:2],collapse='-'))}
 df.ciber.2$ID <- sapply(strsplit(df.ciber.2[,1],"-"),paste.s)
 df.ciber.2 <- merge(df.ciber.2, df.cov,by.x = "ID", by.y = 'SUBJID')
-df.ciber.2$AGE <- as.factor(df.ciber.2$AGE)
+# df.ciber.2$AGE <- as.factor(df.ciber.2$AGE)
 
-# CD4
+# merge immune cell types into broader lineages
 s <- 'CD4_Tcells'
 df.ciber.2[,s] <- apply(df.ciber.2[,grepl('CD4',colnames(df.ciber.2))],1,sum)
 s <- 'MacrophageSum'
 df.ciber.2[,s] <- apply(df.ciber.2[,grepl('Macrophage',colnames(df.ciber.2))],1,sum)
 
-f <- paste0(workdir,'CIBERSORT_out/','GTEx_v7_genexpr_','ALL','.CIBERSORT.ABS-',abs,'.QN-F.perm-1000.txt')
-fwrite(df.ciber.2,f)
-
-
+# save
+f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/CIBERSORT_out/GTEx_v7_genexpr_ALL.CIBERSORT.ABS-',absolute,'.QN-F.perm-1000.txt')
+fwrite(df.ciber.2,f,col.names = T,row.names = F,sep='\t',quote=F,na='NA')
 
--- scripts/GTEx_Deconv/xCell_generation.R
+++ scripts/GTEx_Deconv/xCell_generation.R
@@ -1,12 +1,15 @@
 library(xCell)
 library(data.table)
-f <- '/athena/elementolab/scratch/anm2868/GTEx/EXPRESSION/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct'
 print("loading expression...")
+f <- '/athena/elementolab/scratch/anm2868/GTEx/EXPRESSION/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct'
 df.exp <- fread(f,data.table = F,stringsAsFactors = F)
-print("done loading.")
+print("done loading...")
+
+# covariate data
 df.attr <- fread('/athena/elementolab/scratch/anm2868/GTEx/COVAR/GTEx_v7_Annotations_SampleAttributesDS.txt',data.table = F,stringsAsFactors = F)
 
-TPM_df.full <- df.exp[,c(2:ncol(df.exp))]
+# arrange TPM file into right format (merging duplicates into means)
+TPM_df.full <- df.exp[,-1]
 colnames(TPM_df.full)[1] <- 'Gene'
 TPM_df.full <- as.data.table(TPM_df.full)
 TPM_df.full <- TPM_df.full[ , lapply(.SD, mean), by = list(TPM_df.full$Gene)]
@@ -19,19 +22,16 @@ tis.uniq = unique(df.attr$SMTSD)
 ##################################################
 ##################################################
 ##################################################
-# for doing full thing at once
+# for performing full xCell at once: NOT RECOMMENDED. PERFORMS WORSE.
 # Result_CellTypes_xCell <- xCellAnalysis(TPM_df.full,rnaseq = T)
 # Result_CellTypes_xCell.t <- as.data.frame(t(Result_CellTypes_xCell))
 # Result_CellTypes_xCell.t$SAMP <- rownames(Result_CellTypes_xCell.t); rownames(Result_CellTypes_xCell.t) <- NULL
 # 
-# fwrite(Result_CellTypes_xCell.t,file = paste0('/athena/elementolab/scratch/anm2868/GTEx/infil_output/XCell_','Marderstein','.txt'),sep='\t',quote=F,row.names=F,col.names=T)
+# fwrite(Result_CellTypes_xCell.t,file = '/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/infiltration_profiles/xcell_together',sep='\t',quote=F,row.names=F,col.names=T)
 ##################################################
 ##################################################
 ##################################################
 
-##################################################
-##################################################
-##################################################
 # for subsetting by tissue
 for (i in 1:length(tis.uniq)) {
   tis = tis.uniq[i]
@@ -46,7 +46,7 @@ for (i in 1:length(tis.uniq)) {
   Result_CellTypes_xCell.t <- as.data.frame(t(Result_CellTypes_xCell))
   Result_CellTypes_xCell.t$SAMP <- rownames(Result_CellTypes_xCell.t); rownames(Result_CellTypes_xCell.t) <- NULL
   
-  fwrite(Result_CellTypes_xCell.t,file = paste0('/athena/elementolab/scratch/anm2868/GTEx/infil_output/xcell_sub_out/XCell_',tis,'.txt'),sep='\t',quote=F,row.names=F,col.names=T)
+  fwrite(Result_CellTypes_xCell.t,file = paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/infiltration_profiles/xcell_sub_out/XCell_',tis,'.txt'),sep='\t',quote=F,row.names=F,col.names=T)
 }
 ##################################################
 ##################################################
