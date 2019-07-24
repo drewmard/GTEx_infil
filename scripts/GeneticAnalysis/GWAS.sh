c2b5cfabbb5c09836b218d3db3930c1065a8d926
Andrew Marderstein <anm2868@aphrodite.med.cornell.edu>
2019-07-23 22:26:54 -0400

    “init“

---
+++ scripts/GeneticAnalysis/GWAS.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -J GTEx
+#SBATCH -n 1
+#SBATCH --mem=4G
+#SBATCH --array=1-72:1
+
+# Written by Andrew Marderstein (2018-2019). Contact: anm2868@med.cornell.edu
+# Script to run GWAS for identifying iQTLs
+
+source activate HLMM
+
+i=$SLURM_ARRAY_TASK_ID
+geno=/athena/elementolab/scratch/anm2868/GTEx/gtex_all.filter.name
+pheno=/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/gtex_all.filter.name.txt
+outdir=/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS
+
+mkdir -p $outdir
+
+plink --bfile $geno --pheno $pheno --pheno-name pheno$i.1 --assoc --out $outdir/GTEx.pheno$i.1 --allow-no-sex --maf 0.05
+plink --bfile $geno --pheno $pheno --pheno-name pheno$i.2 --assoc --out $outdir/GTEx.pheno$i.2 --allow-no-sex --maf 0.05
+plink --bfile $geno --pheno $pheno --pheno-name pheno$i.3 --assoc --out $outdir/GTEx.pheno$i.3 --allow-no-sex --maf 0.05
+
