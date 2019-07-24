3bf27e5e6d257914c07d8a7a56685faa993d7b92
drewmard <arm286@cornell.edu>
2019-07-23 22:27:39 -0400

    “init“

--- scripts/GeneticAnalysis/Empirical_Brown_pval.R
+++ scripts/GeneticAnalysis/Empirical_Brown_pval.R
@@ -1,3 +1,7 @@
+# Written by Andrew Marderstein (2018-2019). Contact: anm2868@med.cornell.edu
+
+# Script for creating empirical brown p-values
+
 # Packages
 library(data.table)
 library(EmpiricalBrownsMethod)
