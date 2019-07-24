f77075ce316fae873d8d12956f07f6d3b365c7fa
Andrew Marderstein <anm2868@curie.pbtech>
2019-07-24 09:16:41 -0400

    “init“

---
+++ scripts/GeneticAnalysis/Empirical_Brown_pval_wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+#SBATCH -J EBM
+#SBATCH -n 1
+#SBATCH --mem=8G
+#SBATCH --array=1-3:1
+
+spack load -r r@3.5.0
+/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/scripts/GeneticAnalysis/Empirical_Brown_pval.R $i
--- scripts/GeneticAnalysis/GWAS_local.sh
+++ scripts/GeneticAnalysis/GWAS_local.sh
@@ -1,22 +1,21 @@
 #!/bin/bash -l
 #SBATCH -J GTEx
 #SBATCH -n 1
-#SBATCH --tasks-per-node=4
-#SBATCH --mem=4G
-#SBATCH --array=1-72:1
 
 # Written by Andrew Marderstein (2018-2019). Contact: anm2868@med.cornell.edu
 # Script to run GWAS for identifying iQTLs
 
 source activate HLMM
 
-i=$SLURM_ARRAY_TASK_ID
 geno=/athena/elementolab/scratch/anm2868/GTEx/gtex_all.filter.name
 pheno=/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/gtex_all.filter.name.txt
 outdir=/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS
 
 mkdir -p $outdir
 
+
+for i in {1..72};
+do
 if [ ! -f $outdir/GTEx.pheno$i.1.qassoc ]; then
 plink --bfile $geno --pheno $pheno --pheno-name pheno$i.1 --assoc --out $outdir/GTEx.pheno$i.1 --allow-no-sex --maf 0.05
 fi
@@ -26,3 +25,4 @@ fi
 if [ ! -f $outdir/GTEx.pheno$i.3.qassoc ]; then
 plink --bfile $geno --pheno $pheno --pheno-name pheno$i.3 --assoc --out $outdir/GTEx.pheno$i.3 --allow-no-sex --maf 0.05
 fi
+done
