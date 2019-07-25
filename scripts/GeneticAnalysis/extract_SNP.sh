b1a04e42a53762bc33ef1934ada9360b19e89458
Andrew Marderstein <anm2868@curie.pbtech>
2019-07-25 19:30:22 -0400

    “init“

---
+++ scripts/GeneticAnalysis/extract_SNP.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -J snp
+
+#source activate GTEx
+indir=/athena/elementolab/scratch/anm2868/GTEx
+outdir=/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/Genotype_Subset
+prefix=gtex_all
+
+mkdir -p $outdir
+snp=$1 # 1_751756_T_C_b37
+plink=~/miniconda3/envs/GTEx/bin/plink
+$plink --bfile $indir/$prefix --snp $snp --recodeA --out $outdir/${prefix}.$snp
+
