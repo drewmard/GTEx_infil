e08bdb2c5b2acc9dc2f80a7f1f79d093a7ccb296
Andrew Marderstein <anm2868@curie.pbtech>
2019-07-25 11:59:38 -0400

    “init“

--- scripts/GeneticAnalysis/GWAS.sh
+++ scripts/GeneticAnalysis/GWAS.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1
 #SBATCH --tasks-per-node=4
 #SBATCH --mem=4G
-#SBATCH --array=1-72:1
+#SBATCH --array=1-14:1
 
 # Written by Andrew Marderstein (2018-2019). Contact: anm2868@med.cornell.edu
 # Script to run GWAS for identifying iQTLs
@@ -17,12 +17,14 @@ outdir=/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysi
 
 mkdir -p $outdir
 
-if [ ! -f $outdir/GTEx.pheno$i.1.qassoc ]; then
-plink --bfile $geno --pheno $pheno --pheno-name pheno$i.1 --assoc --out $outdir/GTEx.pheno$i.1 --allow-no-sex --maf 0.05
-fi
-if [ ! -f $outdir/GTEx.pheno$i.2.qassoc ]; then
-plink --bfile $geno --pheno $pheno --pheno-name pheno$i.2 --assoc --out $outdir/GTEx.pheno$i.2 --allow-no-sex --maf 0.05
-fi
-if [ ! -f $outdir/GTEx.pheno$i.3.qassoc ]; then
-plink --bfile $geno --pheno $pheno --pheno-name pheno$i.3 --assoc --out $outdir/GTEx.pheno$i.3 --allow-no-sex --maf 0.05
-fi
+plink --bfile $geno --pheno $pheno --all-pheno --assoc --out $outdir/GTEx.pheno --allow-no-sex --maf 0.05 --chr $i
+
+# if [ ! -f $outdir/GTEx.pheno$i.1.qassoc ]; then
+# plink --bfile $geno --pheno $pheno --pheno-name pheno$i.1 --assoc --out $outdir/GTEx.pheno$i.1 --allow-no-sex --maf 0.05 --chr $i
+# fi
+# if [ ! -f $outdir/GTEx.pheno$i.2.qassoc ]; then
+# plink --bfile $geno --pheno $pheno --pheno-name pheno$i.2 --assoc --out $outdir/GTEx.pheno$i.2 --allow-no-sex --maf 0.05
+# fi
+# if [ ! -f $outdir/GTEx.pheno$i.3.qassoc ]; then
+# plink --bfile $geno --pheno $pheno --pheno-name pheno$i.3 --assoc --out $outdir/GTEx.pheno$i.3 --allow-no-sex --maf 0.05 --chr $i
+# fi
