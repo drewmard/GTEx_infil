31fb4c48b46b068898c324215f4844045b4b011e
drewmard <arm286@cornell.edu>
2020-01-21 08:05:40 -0700

    “update“

--- scripts/GeneticAnalysis/shuffled_data/Empirical_Brown_pval_shuffled.R
+++ scripts/GeneticAnalysis/shuffled_data/Empirical_Brown_pval_shuffled.R
@@ -11,13 +11,13 @@ library(parallel)
 args = commandArgs(trailingOnly=TRUE)
 i <- as.numeric(args[1]) # what pheno to look at?
 
-f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/GTEx.pheno',i,'.ALL_EBM.sig.txt')
+f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/shuffled_data/GTEx.pheno',i,'.ALL_EBM.sig.txt')
 # if (!file.exists(f)) {
 if (1==1) {
   
   # Read in infiltration data
   infil_pheno <- fread('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/infiltration_phenotypes.txt',data.table=F,stringsAsFactors = F)
-  df.pheno <- fread('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/gtex_all.filter.name.txt',data.table=F,header=T,stringsAsFactors = F)
+  df.pheno <- fread('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/gtex_all.filter.name.shuffled_data.txt',data.table=F,header=T,stringsAsFactors = F)
   
   # initialize
   tis <- infil_pheno$tissue[i]
@@ -27,13 +27,13 @@ if (1==1) {
 
   # cibersort rel
   print('CIBERSORT (relative)')
-  df1 <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/GTEx.pheno',i,'.1.qassoc'),data.table=F,stringsAsFactors = F)
+  df1 <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/shuffled_data/GTEx.pheno',i,'.1.qassoc'),data.table=F,stringsAsFactors = F)
   df1 <- df1[,c('CHR','SNP','BP','NMISS','BETA','P')] 
   colnames(df1)[(ncol(df1)-1):ncol(df1)] <- c('beta_cibersort_rel','p_lrt_cibersort_rel')
   
   # CIBERSORT (absolute)
   print('CIBERSORT (absolute)')
-  df2 <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/GTEx.pheno',i,'.2.qassoc'),data.table=F,stringsAsFactors = F)
+  df2 <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/shuffled_data/GTEx.pheno',i,'.2.qassoc'),data.table=F,stringsAsFactors = F)
   df2 <- df2[,c('SNP','BETA','P')]
   colnames(df2)[2:3] <- c('beta_cibersort_abs','p_lrt_cibersort_abs')
   
@@ -41,7 +41,7 @@ if (1==1) {
   
   # xcell
   print('xcell')
-  df3 <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/GTEx.pheno',i,'.3.qassoc'),data.table=F,stringsAsFactors = F)
+  df3 <- fread(paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/shuffled_data/GTEx.pheno',i,'.3.qassoc'),data.table=F,stringsAsFactors = F)
   df3 <- df3[,c('SNP','BETA','P')]
   colnames(df3)[2:3] <- c('beta_xCell','p_lrt_xCell')
   
@@ -79,12 +79,12 @@ if (1==1) {
   df.all$Pval_Brown_Abs <- p
   
   print('Writing...')  
-  f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/GTEx.pheno',i,'.ALL_EBM.txt')
+  f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/shuffled_data/GTEx.pheno',i,'.ALL_EBM.txt')
   fwrite(df.all,f,sep='\t',row.names = F,col.names = T,quote = F)
   
   print('Subsetting significant associations...')  
   df.sub <- subset(df.all,Pval_Brown < 1e-5 | Pval_Brown_Abs < 1e-5 | p_lrt_cibersort_rel < 1e-5 | p_lrt_cibersort_abs < 1e-5 | p_lrt_xCell < 1e-5 )
-  f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/GTEx.pheno',i,'.ALL_EBM.sig.txt')
+  f <- paste0('/athena/elementolab/scratch/anm2868/GTEx/GTEx_infil/output/GeneticAnalysis/GWAS/shuffled_data/GTEx.pheno',i,'.ALL_EBM.sig.txt')
   fwrite(df.sub,f,sep='\t',row.names = F,col.names = T,quote = F)
   
 }
